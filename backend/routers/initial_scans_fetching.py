d90bb7253c7bffa2284ebb8c3fc86ba23b02915e
Anas321 <nassar@lbl.gov>
2025-05-09 16:10:12 -0700

    Added the comparison with pyFAI GISAXS equations file

--- backend/routers/initial_scans_fetching.py
+++ backend/routers/initial_scans_fetching.py
@@ -18,6 +18,9 @@ async def get_initial_scans(left_image_index: int = 0, right_image_index: int = 
 
     DEV_MODE = False
 
+    if DEV_MODE:
+        right_image_index = 0
+
     # Load the .env file
     load_dotenv("../.env")
     # Get the values of TILED_URI and MASK_FILE_NAME
@@ -25,32 +28,33 @@ async def get_initial_scans(left_image_index: int = 0, right_image_index: int = 
     mask_uri = os.getenv("TILED_URI_MASK")  # "MASK_FILE_NAME")
 
     mask_file_name = mask_uri.split("/")[-1]
-    tiled_api_key = os.getenv("TILED_API_KEY")
+    tiled_api_key_images = os.getenv("TILED_API_KEY_IMAGES")
+    tiled_api_key_mask = os.getenv("TILED_API_KEY_MASK")
 
-    if not tiled_uri or not mask_uri or not tiled_api_key:
+    if not tiled_uri or not mask_uri or not tiled_api_key_images:
         raise HTTPException(
             status_code=500, detail="Environment variables not set correctly"
         )
 
-    # data_local_path = "./new_camera"  # "./SALT_DATA"
-    data_local_path = "../SALT_DATA"
+    data_local_path = "../new_camera"  # "./SALT_DATA"
+    # data_local_path = "../SALT_DATA"
 
     if DEV_MODE:
         data_files_type = ".edf"
-        # mask_file_name = "new_mask.npy"  # "mask.npy"
-        mask_file_name = "mask.npy"
+        mask_file_name = "new_mask.npy"  # "mask.npy"
+        # mask_file_name = "mask.npy"
         all_files_uris, mask_detector = get_local_files_names(
             data_local_path, data_files_type, mask_file_name
         )
     else:
 
-        tiled_client = from_uri(tiled_uri, api_key=tiled_api_key)
+        tiled_client = from_uri(tiled_uri, api_key=tiled_api_key_images)
 
         TILED_BASE_URI = tiled_client.uri
         scan_options = get_scan_options(tiled_client, TILED_BASE_URI)
         all_files_uris = scan_options
 
-        mask_client = from_uri(mask_uri)
+        mask_client = from_uri(mask_uri, api_key=tiled_api_key_mask)
         mask_detector = mask_client.read()  # This retrieves the actual NumPy array
         all_files_uris = [file_name.replace("/", "", 1) for file_name in all_files_uris]
         all_files_uris = [file for file in all_files_uris if file != mask_file_name]
